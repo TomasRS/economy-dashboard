fb544b455908442f4d8aa05927a535e8c89b0245
Tomas Rodriguez Saavedra <tomas_rs@hotmail.com>
2020-06-28 22:09:15 -0300

    small refactor of http service injection into body component instead of app component

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './services/data.service';
-import { DollarType} from './models/dollartype.model';
-import { CentralBank } from './models/centralbank.model';
 
 @Component({
   selector: 'app-root',
@@ -9,39 +6,12 @@ import { CentralBank } from './models/centralbank.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
-  mockUsd: DollarType = { name: "Cargando nombre...", buyValue: "Cargando...", sellValue: "Cargando...", imagePath:"../assets/loading-dollar-image.jpg" };
-  usds: DollarType[] = [this.mockUsd, this.mockUsd, this.mockUsd, this.mockUsd];
-  annualExpectedInflations: CentralBank[] = [];
   
   title = 'webapp';
 
-  constructor(private dataService: DataService){
-    
+  constructor(){
   }
 
   ngOnInit(){
-    this.getUsdValues();
-    this.getAnnualExpectedInflation();
   }
-
-  public getUsdValues(){
-    this.dataService.getUsdValues().subscribe((data: any[])=>{
-      this.usds = data;
-      this.usds.forEach(usd => usd.buyValue = "$"+usd.buyValue);
-      this.usds.forEach(usd => usd.sellValue = "$"+usd.sellValue);
-      this.usds[0].imagePath = "../assets/100_dolar_bill_compressed.jpg";
-      this.usds[1].imagePath = "../assets/100_dolar_blue_bill_compressed.jpg";
-      this.usds[2].imagePath = "../assets/100_dolar_ccl_bill_compressed.jpg";
-      this.usds[3].imagePath = "../assets/100_dolar_ccl_bill_compressed.jpg";
-    })
-  }
-
-  public getAnnualExpectedInflation(){
-    this.dataService.getAnnualExpectedInflation().subscribe((data: any[])=>{
-      this.annualExpectedInflations = data;
-      this.annualExpectedInflations = this.annualExpectedInflations.slice(this.annualExpectedInflations.length - 10);
-    })
-  }
-
 }
--- webapp/src/app/body/body.component.ts
+++ webapp/src/app/body/body.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DollarType } from '../models/dollartype.model';
 import { CentralBank } from '../models/centralbank.model';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-body',
@@ -8,12 +9,13 @@ import { CentralBank } from '../models/centralbank.model';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
+  
+  mockUsd: DollarType = { name: "Cargando nombre...", buyValue: "Cargando...", sellValue: "Cargando...", imagePath:"../assets/loading-dollar-image.jpg" };
+  usds: DollarType[] = [this.mockUsd, this.mockUsd, this.mockUsd, this.mockUsd];
+  annualExpectedInflations: CentralBank[] = [];
 
   public annualExpectedInflationValues: string[] = [];
   public annualExpectedInflationDates: string[] = [];
-
-  @Input() usds: DollarType[];
-  @Input() annualExpectedInflations: CentralBank[];
   
   public lineChart: string = 'line';
 
@@ -26,19 +28,37 @@ export class BodyComponent implements OnInit {
     responsive: true
   };
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.getUsdValues();
+    this.getAnnualExpectedInflation();
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.annualExpectedInflationValues = changes.annualExpectedInflations.currentValue.map(dateValue => dateValue.value);
-    this.annualExpectedInflationDates = changes.annualExpectedInflations.currentValue.map(dateValue => dateValue.date);
-    
-    this.expectedVsInterannualInflationDataset = [
-    {data: this.annualExpectedInflationValues, label:'Inflacion anual esperada'},
-    {data: this.annualExpectedInflationValues, label:'Inflacion interanual'}];
+  public getUsdValues(){
+    this.dataService.getUsdValues().subscribe((data: any[])=>{
+      this.usds = data;
+      this.usds.forEach(usd => usd.buyValue = "$"+usd.buyValue);
+      this.usds.forEach(usd => usd.sellValue = "$"+usd.sellValue);
+      this.usds[0].imagePath = "../assets/100_dolar_bill_compressed.jpg";
+      this.usds[1].imagePath = "../assets/100_dolar_blue_bill_compressed.jpg";
+      this.usds[2].imagePath = "../assets/100_dolar_ccl_bill_compressed.jpg";
+      this.usds[3].imagePath = "../assets/100_dolar_ccl_bill_compressed.jpg";
+    })
+  }
 
-    this.expectedVsInterannualInflationLabels = this.annualExpectedInflationDates;
+  public getAnnualExpectedInflation(){
+    this.dataService.getAnnualExpectedInflation().subscribe((data: any[])=>{
+      this.annualExpectedInflations = data;
+      this.annualExpectedInflations = this.annualExpectedInflations.slice(this.annualExpectedInflations.length - 10);
+      this.annualExpectedInflationValues = this.annualExpectedInflations.map(dateValue => dateValue.value);
+      this.annualExpectedInflationDates = this.annualExpectedInflations.map(dateValue => dateValue.date);
+    
+      this.expectedVsInterannualInflationDataset = [
+      {data: this.annualExpectedInflationValues, label:'Inflacion anual esperada'},
+      {data: this.annualExpectedInflationValues, label:'Inflacion interanual'}];
+
+      this.expectedVsInterannualInflationLabels = this.annualExpectedInflationDates;
+    });
   }
 }
